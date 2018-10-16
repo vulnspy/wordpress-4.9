d83103d7c996a06db9fe9355c5c42213d9823e08
Ambulong <zeng.ambulong@gmail.com>
2018-10-17 01:40:45 +0800

    u

--- wordpress/wp/wp-config.php
+++ wordpress/wp/wp-config.php
@@ -33,7 +33,7 @@ define('DB_USER', 'wordpress');
 define('DB_PASSWORD', 'wordpress');
 
 /** MySQL hostname */
-define('DB_HOST', 'db:3306');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
